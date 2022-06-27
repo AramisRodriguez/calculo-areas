379f920e1f1f4bddf5d10b8366891ca89a461e75
Aramis <aramisrodriguez696@gmail.com>
2022-06-27 20:31:54 -0300

    last commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,61 @@
-function auto(marca, modelo, annio) {
-    this.marca = marca;
-    this.modelo = modelo;
-    this.annio = annio;
+
+const PI = Math.PI;
+
+
+
+function calculoAreaCuadrado(lado) {
+    let area = lado * lado;
+    return area;
 }
 
-let listaAutos = [];
-
-function agregarAuto (marca, modelo, annio) {
-    let autoNuevo = new auto(marca, modelo, annio);
-    listaAutos.push(autoNuevo);
+function calculoAreaRectangulo(ancho, largo) {
+    let area = ancho * largo;
+    return area;
 }
 
-for (i = 1; i <= 30; i++) {
-    agregarAuto(i, i, i);
+function calculoAreaCirculo(radio) {
+    let radioCuadrado = radio * radio;
+    let area = PI * radioCuadrado;
+    return area;
 }
 
-document.write(listaAutos[0].marca);
+
+
+
+function areaCuadrado() {
+    let input = document.getElementById("cuadrado");
+    let value = Number(input.value);
+
+    let result = document.getElementById("resultCuadrado");
+
+    let area = calculoAreaCuadrado(value);
+
+    result.innerText = `El área del cuadrado es ${area}cm^2`;
+}
+
+
+function areaRectangulo() {
+    let inputAncho = document.getElementById("rectanguloAncho");
+    let valueAncho = Number(inputAncho.value);
+
+    let inputLargo = document.getElementById("rectanguloLargo");
+    let valueLargo = Number(inputLargo.value);
+
+    let result = document.getElementById("resultRectangulo");
+
+    let area = calculoAreaRectangulo(valueAncho, valueLargo);
+
+    result.innerText = `El área del rectángulo es ${area}cm^2`;
+}
+
+
+function areaCirculo() {
+    let input = document.getElementById("circulo");
+    let value = Number(input.value);
+
+    let result = document.getElementById("resultCirculo");
+
+    let area = calculoAreaCirculo(value);
+
+    result.innerText = `El área del circulo es ${area}cm^2`
+}
